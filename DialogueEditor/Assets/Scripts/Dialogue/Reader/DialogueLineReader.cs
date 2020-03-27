5c82a9bc87e9a3a03f3a2de9c690037f20bac9df
Alexis <thiebautalexis01@gmail.com>
2020-03-27 18:14:29 +0100

    Add New Component to play one line

---
+++ DialogueEditor/Assets/Scripts/Dialogue/Reader/DialogueLineReader.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Linq; 
+using UnityEngine;
+using TMPro;
+using MoonSharp.Interpreter;
+
+namespace DialogueEditor
+{
+    public class DialogueLineReader : MonoBehaviour
+    {
+        #region Fields and Properties
+        [SerializeField] private string m_lineDescriptorName = string.Empty;
+        [SerializeField] private string m_dialogueLineKey = string.Empty;
+        [SerializeField] private float m_intialWaitingTime = 0;
+
+        [SerializeField] private TMP_Text m_textDisplayer = null;
+        [SerializeField] private Color m_fontColor = Color.black;
+        [SerializeField] private AudioSource m_audioSource = null;
+
+        [SerializeField] private UnityEngine.Events.UnityEvent m_event = new UnityEngine.Events.UnityEvent();
+        [SerializeField] private ConditionEvent[] m_conditionEvents = new ConditionEvent[] { }; 
+
+        private Script m_lineDescriptor = null; 
+        #endregion
+
+        #region Methods
+
+        #region Original Methods
+        /// <summary>
+        /// Once the line descritpros are loaded, get its linedescriptor
+        /// </summary>
+        private void OnLineDescriptorLoaded()
+        {
+            if (DialogueAssetsManager.LineDescriptorsTextAsset.Any(a => a.name == m_lineDescriptorName))
+            {
+                m_lineDescriptor = new Script();
+                m_lineDescriptor.DoString(DialogueAssetsManager.LineDescriptorsTextAsset.Where(a => a.name == m_lineDescriptorName).First().ToString());
+            }
+        }
+
+        /// <summary>
+        /// Play the line once with the associated color and Audio Clip
+        /// Then clear the text
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator PlayLine()
+        {
+            if (!m_textDisplayer) yield break;
+            // Change the color of the font if needed
+            if (!DialoguesSettingsManager.DialogsSettings.OverrideCharacterColor)
+            {
+                if (DialoguesSettingsManager.DialogsSettings.CharactersColor.Any(c => c.CharacterIdentifier == m_dialogueLineKey.Substring(0, 2)))
+                    m_textDisplayer.color = DialoguesSettingsManager.DialogsSettings.CharactersColor.Where(c => c.CharacterIdentifier == m_dialogueLineKey.Substring(0, 2)).Select(c => c.CharacterColor).FirstOrDefault();
+                else m_textDisplayer.color = m_fontColor;
+            }
+            else
+                m_textDisplayer.color = m_fontColor;
+            // Change the text of the text displayer
+            m_textDisplayer.text = GetDialogueLineContent(m_dialogueLineKey, DialoguesSettingsManager.DialogsSettings.CurrentLocalisationKey);
+            // If there is an audiosource and the AudioClip Exists in the DialogsAssetsManager, play the audioclip in OneShot
+            if (m_audioSource != null && DialogueAssetsManager.DialogLinesAudioClips.ContainsKey(m_dialogueLineKey + "_" + DialoguesSettingsManager.DialogsSettings.CurrentAudioLocalisationKey))
+            {
+                AudioClip _c = DialogueAssetsManager.DialogLinesAudioClips[m_dialogueLineKey + "_" + DialoguesSettingsManager.DialogsSettings.CurrentAudioLocalisationKey];
+
+                m_audioSource.PlayOneShot(_c);
+                yield return new WaitForSeconds(_c.length + .05f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(m_intialWaitingTime);
+            }
+            m_event?.Invoke();
+            m_conditionEvents.ToList().ForEach(e => DialoguesSettingsManager.SetConditionBoolValue(e.ConditionName, e.ConditionValue));
+            m_textDisplayer.text = string.Empty;
+        }
+
+        /// <summary>
+        /// Get the Content of the Dialog Line according to the localisationKey Selected
+        /// </summary>
+        /// <param name="_dialogLineID">ID of the Dialog Line</param>
+        /// <param name="_localisationKey">Localisation Key to use</param>
+        /// <returns></returns>
+        public string GetDialogueLineContent(string _dialogLineID, string _localisationKey)
+        {
+            if (_dialogLineID == string.Empty) return string.Empty;
+            DynValue _content = m_lineDescriptor.Globals.Get(_dialogLineID).Table.Get(_localisationKey);
+            return _content.String;
+        }
+        #endregion
+
+        #region Unity Methods
+        private void Awake()
+        {
+            DialogueAssetsManager.LineDescriptorsLoadedCallBack += OnLineDescriptorLoaded;
+        }
+        #endregion 
+
+        #endregion
+    }
+}
+
---
+++ DialogueEditor/Assets/Scripts/Editor/ReaderEditor/DialogueLineReaderEditor.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq; 
+using UnityEngine;
+using UnityEditor;
+using MoonSharp.Interpreter; 
+
+namespace DialogueEditor
+{
+    [CustomEditor(typeof(DialogueLineReader))]
+    public class DialogueLineReaderEditor : Editor
+    {
+        #region Fields and Properties
+        private SerializedProperty m_lineDescriptorName = null;
+        private SerializedProperty m_dialogueLineKey = null;
+        private SerializedProperty m_intialWaitingTime = null;
+
+        private SerializedProperty m_textDisplayer = null;
+        private SerializedProperty m_fontColor = null;
+        private SerializedProperty m_audioSource = null;
+
+        private SerializedProperty m_event = null;
+        private SerializedProperty m_conditionEvents = null; 
+
+        private string m_content = string.Empty; 
+        private string[] m_lineDescriptors = null;
+        private string[] m_linesKey = null;
+        private string[] m_conditions = new string[] { };
+        private int m_lineDescriptorIndex = -1;
+        private int m_dialogueLineKeyIndex = -1; 
+        private GUIStyle m_style = null; 
+        #endregion
+
+        #region Methods
+
+        #region Original Methods
+        /// <summary>
+        /// Draw the Inspector
+        /// </summary>
+        private void DrawEditor()
+        {
+            EditorGUILayout.LabelField(new GUIContent("Dialogue Line"), m_style);
+            EditorGUI.BeginChangeCheck();
+            m_lineDescriptorIndex = EditorGUILayout.Popup(new GUIContent("Line Descriptor"), m_lineDescriptorIndex, m_lineDescriptors); 
+            if(EditorGUI.EndChangeCheck())
+            {
+                m_lineDescriptorName.stringValue = m_lineDescriptors[m_lineDescriptorIndex];
+                InitLineKeys(); 
+            }
+            EditorGUI.BeginChangeCheck();
+            m_dialogueLineKeyIndex = EditorGUILayout.Popup(new GUIContent("Line Descriptor"), m_dialogueLineKeyIndex, m_linesKey);
+            if (EditorGUI.EndChangeCheck())
+            {
+                m_dialogueLineKey.stringValue = m_linesKey[m_dialogueLineKeyIndex];
+                m_content = GetContent(); 
+            }
+            Color _originalColor = GUI.color;
+            if (DialoguesSettingsManager.DialogsSettings.CharactersColor.Any(c => c.CharacterIdentifier == m_dialogueLineKey.stringValue.Substring(0,2)))
+                GUI.color = DialoguesSettingsManager.DialogsSettings.CharactersColor.Where(c => c.CharacterIdentifier == m_dialogueLineKey.stringValue.Substring(0, 2)).Select(c => c.CharacterColor).FirstOrDefault();
+            EditorGUILayout.TextArea(m_content);
+            GUI.color = _originalColor; 
+
+            EditorGUILayout.LabelField(new GUIContent("Settings"), m_style);
+            m_intialWaitingTime.floatValue = EditorGUILayout.Slider(new GUIContent("Initial Waiting Time", "Used only if the dialogue line has no Audio Asset linked to it."), m_intialWaitingTime.floatValue, .1f, 5.0f);
+            EditorGUILayout.PropertyField(m_textDisplayer);
+            EditorGUILayout.PropertyField(m_audioSource);
+            EditorGUILayout.PropertyField(m_fontColor);
+
+            EditorGUILayout.LabelField(new GUIContent("Events"), m_style);
+            EditorGUILayout.PropertyField(m_event);
+
+            GUILayout.Space(10);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Changing Condition Events");
+            if (GUILayout.Button("+", GUILayout.Width(Screen.width / 8)))
+            {
+                m_conditionEvents.InsertArrayElementAtIndex(0);
+            }
+            GUILayout.EndHorizontal();
+            for (int j = 0; j < m_conditionEvents.arraySize; j++)
+            {
+                SerializedProperty _subProperty = m_conditionEvents.GetArrayElementAtIndex(j);
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("-", GUILayout.Width(15), GUILayout.Height(12)))
+                {
+                    m_conditionEvents.DeleteArrayElementAtIndex(j);
+                    continue;
+                }
+                int _index = m_conditions.ToList().Contains(_subProperty.FindPropertyRelative("m_conditionName").stringValue) ? m_conditions.ToList().IndexOf(_subProperty.FindPropertyRelative("m_conditionName").stringValue) : -1;
+                EditorGUI.BeginChangeCheck();
+                _index = EditorGUILayout.Popup("Set Condition", _index, m_conditions);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    _subProperty.FindPropertyRelative("m_conditionName").stringValue = m_conditions[_index];
+                }
+                GUILayout.EndHorizontal();
+                _subProperty.FindPropertyRelative("m_conditionValue").boolValue = EditorGUILayout.Toggle("To the value", _subProperty.FindPropertyRelative("m_conditionValue").boolValue);
+                GUILayout.Space(5);
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        /// <summary>
+        /// Get all line keys in the LineDescriptor
+        /// </summary>
+        private void InitLineKeys()
+        {
+            Script _luaScript = new Script();
+            string _lineDescriptor = File.ReadAllText(Path.Combine(Dialogue.LineDescriptorPath, m_lineDescriptorName.stringValue + ".txt"));
+            _luaScript.DoString(_lineDescriptor);
+            List<DynValue> _dynValues = _luaScript.Globals.Values.Where(v => (v.Type == DataType.Table) && (v.Table.Get("ID").IsNotNil())).ToList();
+            m_linesKey = _dynValues.Select(v => v.Table.Get("ID").String).ToArray();
+            if (m_dialogueLineKey.stringValue != string.Empty && m_linesKey.Contains(m_dialogueLineKey.stringValue))
+            {
+                m_dialogueLineKeyIndex = m_linesKey.ToList().IndexOf(m_dialogueLineKey.stringValue);
+                m_content = GetContent();
+            }
+            else m_dialogueLineKeyIndex = -1; 
+        }
+
+        /// <summary>
+        /// Get the content of the current KeyIndex in the LineDescriptor
+        /// </summary>
+        /// <returns></returns>
+        private string GetContent()
+        {
+            Script _luaScript = new Script();
+            string _lineDescriptor = File.ReadAllText(Path.Combine(Dialogue.LineDescriptorPath, m_lineDescriptorName.stringValue + ".txt"));
+            _luaScript.DoString(_lineDescriptor);
+            List<DynValue> _dynValues = _luaScript.Globals.Values.Where(v => (v.Type == DataType.Table) && (v.Table.Get("ID").IsNotNil())).ToList();
+            return _dynValues.Where(v => v.Table.Get("ID").String == m_dialogueLineKey.stringValue).Select(v => v.Table.Get(DialoguesSettingsManager.DialogsSettings.CurrentLocalisationKey).String).First();
+        }
+        #endregion
+
+        #region Unity Methods
+        private void OnEnable()
+        {
+            m_style = new GUIStyle();
+            m_style.fontStyle = FontStyle.Bold;
+
+            m_lineDescriptorName = serializedObject.FindProperty("m_lineDescriptorName");
+            m_dialogueLineKey = serializedObject.FindProperty("m_dialogueLineKey");
+            m_intialWaitingTime = serializedObject.FindProperty("m_intialWaitingTime");
+
+            m_textDisplayer = serializedObject.FindProperty("m_textDisplayer");
+            m_fontColor = serializedObject.FindProperty("m_fontColor");
+            m_audioSource = serializedObject.FindProperty("m_audioSource");
+
+            m_lineDescriptors = Directory.GetFiles(Dialogue.LineDescriptorPath, "*" + ".txt").Select(Path.GetFileNameWithoutExtension).ToArray();
+            if(m_lineDescriptorName.stringValue != string.Empty)
+            {
+                m_lineDescriptorIndex = m_lineDescriptors.ToList().IndexOf(m_lineDescriptorName.stringValue);
+                InitLineKeys();
+            }
+
+            m_event = serializedObject.FindProperty("m_event"); 
+            m_conditionEvents = serializedObject.FindProperty("m_conditionEvents");
+            string[] _conditionsLua = DialoguesSettingsManager.DialogsSettings.LuaConditions.Split('\n');
+            m_conditions = new string[_conditionsLua.Length];
+            for (int i = 0; i < _conditionsLua.Length; i++)
+            {
+                m_conditions[i] = _conditionsLua[i].Split('=')[0].Trim();
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawEditor();
+        }
+        #endregion 
+
+        #endregion
+    }
+}
+
